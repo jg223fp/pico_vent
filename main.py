f3b012d8aeb69af6dfd73290c9cdd5e3978ab4bf
jg223fp <jg223fp@student.lnu.se>
2022-07-05 21:30:13 +0200

    Wrote functions for boot and tests.

--- main.py
+++ main.py
@@ -9,24 +9,75 @@ sensor_pin = Pin(27, Pin.OUT, Pin.PULL_DOWN)
 
 #init values
 red_led.value(0)
+sensor = DHT11(sensor_pin)
 
 #Functions
-def blink_red():
-    print("Red LED flashing...")
-    for i in range(8):
+def boot():
+    """Runs a small test for the sensor and for the motor,
+        Flashes the red LED to indicate success. If the lifght is solid an error has accured.
+    """
+    print("System started")
+    print("Performing tests")
+    test_sensor()
+    test_motor()
+    print("Tests completed")
+    for i in range(6):
         red_led.toggle()
-        time.sleep(0.2)  
+        time.sleep(0.1)
+        
+def test_motor():
+    """Performs a test for the fan
+    """
+    pass
 
+def test_sensor():
+    """Performs a test for the DHT11 sensor.
+        Temperature limits: 0 to 50 C
+        Humidity limits: 20-80 +-5%
+        Max update frequenzy: 1 Hz
+    """
+    t = get_temp()
+    if t < 0 or t > 50: 
+        print("ERROR: Temperature out of range")
+        error()
+    h = get_humidity()
+    if h < 15 or h > 95:  
+        print("ERROR: Humidity out of range")
+        error()
+
+def error():
+    """If an error accours the red LED is turned on with a solid light.
+    """
+    red_led.value(1)
+    while True:
+        pass
+    
+def blink_red():
+    """Flashes the red LED 2 times.
+    """
+    print("Red LED flashing...")
+    for i in range(4):
+        red_led.toggle()
+        time.sleep(0.5)
+        
+def get_temp():
+    """Returns a temperature value from the sensor.
+    """
+    return sensor.temperature
+
+def get_humidity():
+    """Returns a humidity value from the sensor.
+    """
+    return sensor.humidity
+
+    
 #Main
+boot()
+print("Starting main program...")
 while True:  
-    #blink_red()
     utime.sleep(1)
-    sensor = DHT11(sensor_pin)
-    t  = (sensor.temperature)
-    h = (sensor.humidity)
-    print("Temperature: {}".format(sensor.temperature))
-    print("Humidity: {}".format(sensor.humidity))
-    time.sleep(1)
+    print("Humidity: {}".format(get_humidity()))
+    utime.sleep(1)
              
 
     
